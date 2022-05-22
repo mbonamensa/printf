42418ce3556f35553c439ba8266f86aad9eb85b2
Maame <mbonamensa@gmail.com>
2022-05-22 07:46:57 -0700

    add print_long_uns.c

---
+++ print_long_uns.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * prinluns - prints a long unsigned integer
+ * @ap: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+
+int prinluns(va_list ap, char *buf, unsigned int ibuf)
+{
+	unsigned long int int_in, int_temp, i, div;
+
+	int_in = va_arg(ap, unsigned long int);
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handlebuf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
+}
