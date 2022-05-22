88ded05b10f5730c38329d7433f7adb270a5ec48
Maame <mbonamensa@gmail.com>
2022-05-22 07:27:22 -0700

    add print_long_hex

---
+++ print_long_hex.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * prinlhex - prints a long decimal in hexadecimal
+ * @ap: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+
+int prinlhex(va_list ap, char *buf, unsigned int ibuf)
+{
+	long int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(ap, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handlebuf(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = fill_binary_arr(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = fill_hex(binary, hexadecimal, 0, 16);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handlebuf(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
